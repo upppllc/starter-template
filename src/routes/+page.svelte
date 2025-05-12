fcfc84cd2c64fac975f5d324b753136134e97f94
24jr <34662182+24jr@users.noreply.github.com>
2025-05-12 17:05:44 -0400

    package updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,13 @@
 <svelte:head>
   <title>__APP_NAME__</title>
   <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+  <!-- <meta name="description" content="tbd" />
+  <meta property="og:title" content="__APP_NAME__" />
+  <meta property="og:description" content="tbd" />
+  <meta property="og:image" content="https://www.example.com/thumbnail.webp" /> 
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:image" content="https://www.example.com/thumbnail.webp" />
+  <link rel="canonical" href="https://www.example.com/" /> -->
 </svelte:head>
 
 <div style="display: grid; place-items: center; margin: 1rem;">
